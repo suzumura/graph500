af6c9a5b0c2c8f38a87a1738cb15b02f96e3e6bd
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2016-05-23 20:03:04 +0900

    add option that continues benchmarking and increases scale unless benchmariking executable returns non-zero code

--- run-benchmark.py
+++ run-benchmark.py
@@ -13,6 +13,7 @@ parser.add_option("-v","--verbose", action="store_true", dest="verboseMode", def
 parser.add_option("--vertex-reordering", action="store", dest="vertexReordering", type="int", default=0)
 parser.add_option("-b", "--bind-mode", action="store", dest="bindMode", type="choice", choices=["NONE", "TRUE", "SPREAD", "CLOSE"], default="NONE")
 parser.add_option("--test", action="store_true", dest="testMode", default=False)
+parser.add_option("--increasing-scale", action="store_true", dest="increasingScale", default=False)
 
 (options, args) = parser.parse_args()
 
@@ -73,21 +74,29 @@ if not sameBuildOptions:
 
 
 # run benchmark
-benchArgs = ["mpirun", "-n", "1"]
+while True:
+    benchArgs = ["mpirun", "-n", "1"]
 
-outFilePattern = "lP1"
-outFilePattern += "T" + str(options.numThreads)
-outFilePattern += "S" + str(options.numScale)
-outFilePattern += "VR" + str(options.vertexReordering)
-outFilePattern += "B" + str(options.bindMode)
-benchArgs.extend(["-outfile-pattern", outFilePattern])
+    outFilePattern = "lP1"
+    outFilePattern += "T" + str(options.numThreads)
+    outFilePattern += "S" + str(options.numScale)
+    outFilePattern += "VR" + str(options.vertexReordering)
+    outFilePattern += "B" + str(options.bindMode)
+    benchArgs.extend(["-outfile-pattern", outFilePattern])
 
-benchArgs.extend(["-genv", "OMP_NUM_THREADS", str(options.numThreads)])
+    benchArgs.extend(["-genv", "OMP_NUM_THREADS", str(options.numThreads)])
 
-if options.bindMode != "NONE":
-    benchArgs.extend(["-genv", "OMP_PROC_BIND", options.bindMode])
+    if options.bindMode != "NONE":
+        benchArgs.extend(["-genv", "OMP_PROC_BIND", options.bindMode])
 
-benchArgs.extend(["./mpi/runnable", str(options.numScale)])
+    benchArgs.extend(["./mpi/runnable", str(options.numScale)])
 
-benchProc = subprocess.Popen(benchArgs)
-benchProc.communicate()
+    benchProc = subprocess.Popen(benchArgs)
+    benchProc.communicate()
+
+    if options.increasingScale and benchProc.returncode == 0:
+        del benchProc
+        options.numScale += 1
+    else:
+        print("return code: " + str(benchProc.returncode))
+        break
